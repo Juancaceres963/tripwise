cd83b03d81254f4136744ffcb95d37247095e2e3
Juancaceres963 <dnijuancaceres@gmail.com>
2023-05-10 10:56:57 -0300

    Enrrutamiento de paginas, ajuste de botones, y nuevo file relato.html

--- js/script.js
+++ js/script.js
@@ -1,61 +1,23 @@
-// Ver mas contenido de Compartir
 
-const verMasCompartir  = document.getElementById('ver_mas_compartir');
-const ocultoCompartir  = document.getElementById('oculto_compartir');
-
-verMasCompartir.addEventListener('click', function (){
-    ocultoCompartir.classList.toggle('mostrar');
-
-    if(ocultoCompartir.classList.contains('mostrar')){
-        verMasCompartir.innerHTML = 'see less.';
-    } else {
-        verMasCompartir.innerHTML = '...see more.';
-    }
-});
-
-
-
-// Ver mas contenido de Conectar con otros viajeros 
-
-const verMasConectar  = document.getElementById('ver_mas_concectar');
-const ocultoConectar  = document.getElementById('oculto_conectar');
-
-verMasConectar.addEventListener('click', function (){
-    ocultoConectar.classList.toggle('mostrar');
-
-    if(ocultoConectar.classList.contains('mostrar')){
-        verMasConectar.innerHTML = 'see less.';
-    } else {
-        verMasConectar.innerHTML = '...see more.';
-    }
-});
-
-// Ver mas contenido de Descubriri un nuevo destino
-
-const verMasEncontar  = document.getElementById('ver_mas_encontrar');
-const ocultoEncontrar = document.getElementById('oculto_Encontrar');
-
-verMasEncontar.addEventListener('click', function (){
-    ocultoEncontrar.classList.toggle('mostrar');
-
-    if(ocultoEncontrar.classList.contains('mostrar')){
-        verMasEncontar.innerHTML = 'see less.';
-    } else {
-        verMasEncontar.innerHTML = '...see more.';
-    }
-});
-
-// Ver mas contenido de Tips de viajeros
-
-const verMasTips  = document.getElementById('ver_mas_tips');
-const ocultoTips = document.getElementById('oculto_tips');
-
-verMasTips.addEventListener('click', function (){
-    ocultoTips.classList.toggle('mostrar');
-
-    if(ocultoTips.classList.contains('mostrar')){
-        verMasTips.innerHTML = 'see less.';
-    } else {
-        verMasTips.innerHTML = '...see more.';
-    }
-});
+function verMas(idBoton, idElemento, textoMas, textoMenos) {
+    const boton = document.getElementById(idBoton);
+    const elemento = document.getElementById(idElemento);
+  
+    boton.addEventListener('click', function() {
+      elemento.classList.toggle('mostrar');
+  
+      if (elemento.classList.contains('mostrar')) {
+        boton.innerHTML = textoMenos;
+      } else {
+        boton.innerHTML = textoMas;
+      }
+    });
+  }
+  
+  verMas('ver_mas_compartir', 'oculto_compartir', '...see more.', 'see less.');
+  verMas('ver_mas_concectar', 'oculto_conectar', '...see more.', 'see less.');
+  verMas('ver_mas_encontrar', 'oculto_Encontrar', '...see more.', 'see less.');
+  verMas('ver_mas_tips', 'oculto_tips', '...see more.', 'see less.');
+  verMas('ver_mas_pascua', 'oculto_pascua', '...see more.', 'see less.');
+  verMas('ver_mas_chitwan', 'oculto_chitwan', '...see more.', 'see less.');
+  verMas('ver_mas_sasso', 'ocultar_sasso', '...see more.', 'see less.');
